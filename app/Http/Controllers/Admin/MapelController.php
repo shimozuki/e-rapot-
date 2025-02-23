1da2732bf596c5178f9b33d6b97110844b367f52
robbishimozuki <robbishimozuki@gmail.com>
2025-02-23 14:14:07 +0800

    eee

--- app/Http/Controllers/Admin/MapelController.php
+++ app/Http/Controllers/Admin/MapelController.php
@@ -71,6 +71,7 @@ class MapelController extends Controller
         } else {
             $mapel = Mapel::findorfail($id);
             $data_mapel = [
+                'nama_mapel' => $request->nama_mapel,
                 'ringkasan_mapel' => $request->ringkasan_mapel,
             ];
             $mapel->update($data_mapel);
